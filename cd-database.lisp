4c2bf9358e0162dcb35845fe302434081f20b069
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2024-01-11 12:03:29 +0900

    feat: データベースを操作するクエリを作成

--- cd-database.lisp
+++ cd-database.lisp
@@ -1,3 +1,21 @@
+;; cdのデータを構築するヘルパー関数
+(defun make-cd
+    (title artist rating ripped)
+    (list :title title :artist artist :rating rating :ripped ripped))
+
+;; cdをdbに追加するヘルパー関数
+(defun add-record 
+    (cd) 
+    (push cd *db*))
+
+;; dbの内容を再帰的に出力する関数
+(defun dump-db 
+    () 
+    (dolist 
+        (cd *db*) 
+        (format t "~{~a:~10t~a~%~}~%" cd)))
+
+;; レコードの初期値を入力するユーティリティ関数
 (defun add-test-records 
     () 
     (add-record 
@@ -11,22 +29,7 @@
     (add-record 
         (make-cd "Double Live" "Garth Brooks" 6 nil)))
 
-(defun make-cd
-    (title artist rating ripped)
-    (list :title title :artist artist :rating rating :ripped ripped))
-
-(defun add-record 
-    (cd) 
-    (push cd *db*))
-
-(defun dump-db 
-    () 
-    (dolist 
-        (cd *db*) 
-        (format t "~{~a:~10t~a~%~}~%" cd)))
-
-
-;; 値を順々に入力してもらいながら、新たなCDのレコードを生成する
+;; 値を順々に入力してもらいながら、新たなCDを生成して追加する関数
 (defun add-cds 
     () 
     (loop 
@@ -37,6 +40,7 @@
                 (y-or-n-p "Another? [y/n]: ")) 
             (return))))
 
+;; add-cdsのためのインタフェース
 (defun prompt-for-cd 
     ()
     (make-cd 
@@ -47,12 +51,14 @@
                 (prompt-read "Rating") :junk-allowed t) 0)
         (y-or-n-p "Ripped [y/n]: ")))
 
+;; 標準入力を読み込むヘルパー関数
 (defun prompt-read 
     (prompt) 
     (format *query-io* "~a: " prompt) 
     (force-output *query-io*) 
     (read-line *query-io*))
 
+;; dbをファイルに書き出す関数
 (defun save-db 
     (filename) 
     (with-open-file 
@@ -60,6 +66,7 @@
         (with-standard-io-syntax 
             (print *db* out))))
 
+;; ファイルからdbを読み出す関数
 (defun load-db 
     (filename) 
     (with-open-file 
@@ -67,3 +74,66 @@
         (with-standard-io-syntax 
             (setf *db* 
                 (read in)))))
+
+;; dbからレコードを選択するクエリ
+(defun select 
+    (selector-fn) 
+    (remove-if-not selector-fn *db*))
+
+(defun artist-selector 
+    (artist) #'
+    (lambda 
+        (cd) 
+        (equal 
+            (getf cd :artist) artist)))
+
+(defun where 
+    (&key title artist rating 
+        (ripped nil ripped-p))
+#'
+    (lambda 
+        (cd)
+        (and
+            (if title 
+                (equal 
+                    (getf cd :title) title ) t)
+            (if artist 
+                (equal 
+                    (getf cd :artist) artist ) t)
+            (if rating 
+                (equal 
+                    (getf cd :rating) rating ) t)
+            (if ripped-p 
+                (equal 
+                    (getf cd :ripped) ripped ) t)
+)))
+
+;; 既存のレコードを更新する
+(defun update 
+    (selector-fn &key title artist rating 
+        (ripped nil ripped-p)) 
+    (setf *db* 
+        (mapcar #'
+            (lambda 
+                (row) 
+                (when 
+                    (funcall selector-fn row) 
+                    (if title 
+                        (setf 
+                            (getf row :title) title))
+                    (if artist 
+                        (setf 
+                            (getf row :artist) artist))
+                    (if rating 
+                        (setf 
+                            (getf row :rating) rating))
+                    (if ripped-p 
+                        (setf 
+                            (getf row :ripped) ripped)))
+row)*db*)))
+
+;; 既存のレコードを削除する
+(defun delete-rows 
+    (selecctor-fn) 
+    (setf *db* 
+        (remove-if selector-fn *db*)))
